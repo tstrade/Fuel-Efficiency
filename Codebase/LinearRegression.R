953fc7639546d2dea5d5bdc8e33f650d98e9125b
Thomas Strade <99762399+tstrade@users.noreply.github.com>
2024-11-24 13:37:11 -0500

    lin reg  - tried with log values

--- Codebase/LinearRegression.R
+++ Codebase/LinearRegression.R
@@ -8,6 +8,13 @@ path_model <- "~/DataScience/Fuel-Efficiency/DataSources/Model_CarSpecs_1945to20
 CarSpecs_model <- read.csv(path_model)
 CarSpecs_model <- CarSpecs_model[2:20]
 
+# Replacing data with the average column value
+CarSpecs_model$curb_weight_kg <- replace(CarSpecs_model$curb_weight_kg, is.na(CarSpecs_model$curb_weight_kg), mean(CarSpecs_model$curb_weight_kg, na.rm=TRUE))
+CarSpecs_model$maximum_torque_n_m <- replace(CarSpecs_model$maximum_torque_n_m, is.na(CarSpecs_model$maximum_torque_n_m), mean(CarSpecs_model$maximum_torque_n_m, na.rm=TRUE))
+CarSpecs_model$fuel_grade <- replace(CarSpecs_model$fuel_grade, is.na(CarSpecs_model$fuel_grade), mean(CarSpecs_model$fuel_grade, na.rm=TRUE))
+# This suggest the possibility of revisiting how the data was cleaned
+# If the model is performing poorly, more columns can be added and cleaned in this manner
+
 # ------------------------------------------------------------------------------------------------------------
 
 linreg_length <- lm(Fuel_Economy~length_mm, CarSpecs_model)
@@ -61,17 +68,17 @@ ggplot(CarSpecs_model, aes(x=(length_mm + curb_weight_kg + maximum_torque_n_m + 
 # ------------------------------------------------------------------------------------------------------------
 
 model <- lm(Fuel_Economy~Body_type + number_of_seats + length_mm + curb_weight_kg +
-              maximum_torque_n_m + injection_type + cylinder_layout + number_of_cylinders +
+              log(maximum_torque_n_m) + injection_type + cylinder_layout + number_of_cylinders +
               compression_ratio + engine_type + valves_per_cylinder + boost_type +
-              engine_placement + engine_hp + drive_wheels + number_of_gears +
+              engine_placement + log(engine_hp) + drive_wheels + number_of_gears +
               transmission + fuel_grade, CarSpecs_model)
 
 summary(model)
 
 ggplot(CarSpecs_model, aes(x=Body_type + number_of_seats + length_mm + curb_weight_kg +
-                             maximum_torque_n_m + injection_type + cylinder_layout + number_of_cylinders +
+                             log(maximum_torque_n_m) + injection_type + cylinder_layout + number_of_cylinders +
                              compression_ratio + engine_type + valves_per_cylinder + boost_type +
-                             engine_placement + engine_hp + drive_wheels + number_of_gears +
+                             engine_placement + log(engine_hp) + drive_wheels + number_of_gears +
                              transmission + fuel_grade, CarSpecs_model, y=Fuel_Economy)) +
   geom_point() + 
   geom_smooth(method='lm') +
